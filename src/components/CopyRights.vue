3e298a87d8d21c49d563c53c12586f92d027682c
ZbWeR <1641064392@qq.com>
2023-08-02 22:39:03 +0800

    ✨ feat: 底部版权声明

---
+++ src/components/CopyRights.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full pb-10 text-sm text-center text-slate-500">
+    <p class="w-3/4 h-px mx-auto my-5 bg-slate-500"></p>
+    <p>&copy; Copyrights 2023 一路&微光 All rights By Reserved</p>
+    <a href="https://beian.miit.gov.cn/"><slot></slot></a>
+  </div>
+</template>
