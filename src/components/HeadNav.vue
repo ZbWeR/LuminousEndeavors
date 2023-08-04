9fea433aae96db72a564583294640391702e8e13
ZbWeR <1641064392@qq.com>
2023-08-04 23:31:44 +0800

    ✨ feat: 按钮样式调整

--- src/components/HeadNav.vue
+++ src/components/HeadNav.vue
@@ -1,5 +1,5 @@
 <template>
-  TODO:按钮样式调整
+  <!-- TODO:按钮样式调整 -->
   <div
     class="box-border fixed top-0 z-10 w-full h-16 text-white shadow-[0_2px_5px_rgba(255,255,255,.3)] bg-gradient-to-r from-[#257afa] to-[#2a303a]"
   >
@@ -8,11 +8,15 @@
         <img src="../assets/logo.png" alt="" class="h-full py-2" />
         <h1 class="font-sans text-2xl">光 点 计 划</h1>
       </div>
-      <div class="flex items-center h-full gap-8">
-        <button class="h-full transition-all hover:bg-[rgba(0,0,0,.1)]">
+      <div class="flex items-center h-full gap-4">
+        <button
+          class="h-full px-4 transition-all hover:scale-110 hover:bg-teal-400"
+        >
           立即报名
         </button>
-        <button class="h-full transition-all hover:bg-teal-800">
+        <button
+          class="h-full px-4 transition-all hover:scale-110 hover:bg-teal-400"
+        >
           查看方向
         </button>
       </div>
