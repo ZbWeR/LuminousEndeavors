c01dbae4ab2655a97687eff89401eec91a133cf6
ZbWeR <1641064392@qq.com>
2023-08-06 23:09:56 +0800

    ✨ feat: 修改标题 & 添加方向介绍页

---
+++ src/views/ProjectIntroduction.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-full bg-zinc-100">
+    <h1
+      class="container pt-16 pb-2 mx-auto font-serif tracking-widest border-b-2 border-blue-400 lg:text-3xl"
+    >
+      关 于 光 点 计 划 - 方 向 介 绍
+    </h1>
+  </div>
+</template>
+
+<script setup>
+</script>
