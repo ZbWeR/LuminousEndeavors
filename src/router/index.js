9019d72bec631054346d22d22169f82221fd80f5
ZbWeR <1641064392@qq.com>
2023-08-08 18:42:19 +0800

    🌈 style: 完成鉴权页面登录部分

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,23 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/ProjectIntroduction.vue')
-  }
+  },
+  {
+    path: '/more',
+    name: 'more',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "more" */ '../views/MoreInfo.vue')
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "more" */ '../views/LoginView.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/LoginView.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="flex items-center justify-center w-screen h-screen bg-sky-50">
+    <div
+      class="flex w-3/5 overflow-hidden bg-sky-100 rounded-xl h-2/3 rootShadow"
+    >
+      <!-- 登录 -->
+      <div class="flex flex-col w-2/3 h-full bg-sky-50">
+        <form
+          action=""
+          method="post"
+          class="flex flex-col items-center justify-center w-full h-full"
+        >
+          <h1
+            class="mb-20 font-mono text-4xl font-bold tracking-widest text-center"
+          >
+            Log In
+          </h1>
+          <input
+            class="block w-1/2 px-4 py-2 tracking-wider duration-300 rounded-lg outline-none placeholder:text-sm inputShadow bg-sky-50"
+            type="text"
+            placeholder="Number"
+          />
+          <input
+            class="block w-1/2 px-4 py-2 mt-4 tracking-wider duration-300 rounded-lg outline-none inputShadow bg-sky-50 placeholder:text-sm"
+            type="password"
+            placeholder="Password"
+          />
+          <div class="relative w-1/2 mt-3 cursor-pointer">
+            <p
+              class="absolute right-0 inline-block text-sm transition-all border-b select-none border-slate-400 hover:text-black text-slate-400"
+            >
+              忘记密码?
+            </p>
+          </div>
+          <button
+            class="box-border w-1/4 px-1 py-3 mt-12 tracking-[0.5em] indent-[0.5em] text-white transition-all rounded-full hover:bg-sky-500 hover:scale-95 bg-sky-400"
+          >
+            登录
+          </button>
+          <!-- 切换登录方式 -->
+          <div class="w-3/5 mt-14">
+            <div class="flex items-center justify-around">
+              <i class="w-1/3 h-px bg-slate-300"></i>
+              <p class="text-sm text-slate-400 shrink-0">其他登录方式</p>
+              <i class="w-1/3 h-px bg-slate-300"></i>
+            </div>
+            <div class="flex justify-center gap-5 mt-4">
+              <!-- 微信登陆 -->
+              <svg
+                class="cursor-pointer w-9 h-9 icon fill-emerald-400"
+                title="微信"
+                aria-hidden="true"
+              >
+                <use xlink:href="#icon-weixindenglu"></use>
+              </svg>
+              <!-- 验证码登录 -->
+              <svg
+                class="cursor-pointer w-9 h-9 icon fill-sky-400"
+                aria-hidden="true"
+              >
+                <use xlink:href="#icon-account_sms"></use>
+              </svg>
+            </div>
+          </div>
+        </form>
+      </div>
+      <!-- 注册 -->
+      <div class="w-1/3 h-full bg-pink-100"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.rootShadow {
+  box-shadow: 6px 6px 8px #d1d9e6, -6px -6px 8px rgba(220, 220, 220, 0.7);
+}
+.inputShadow {
+  box-shadow: inset 2px 2px 4px rgba(0, 0, 0, 0.3),
+    inset -3px -3px 6px rgba(220, 220, 220, 0.7);
+}
+.inputShadow:focus,
+.inputShadow:active {
+  box-shadow: inset 3px 3px 6px rgba(0, 0, 0, 0.5),
+    inset -3px -3px 6px rgba(255, 255, 255, 0.7);
+}
+</style>
