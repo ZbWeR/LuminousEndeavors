13eaf38c14e6c65e9a33a9f3588a5825a539397f
ZbWeR <1641064392@qq.com>
2023-08-08 11:23:42 +0800

    🐞 fix: 修改数据结构优化显示

--- src/views/ProjectIntroduction.vue
+++ src/views/ProjectIntroduction.vue
@@ -1,27 +1,7 @@
 <template>
   <div class="w-full bg-zinc-100">
     <!-- 标题栏 -->
-    <div
-      class="container flex items-center justify-between pt-16 pb-2 mx-auto font-serif border-b-2 border-blue-400"
-    >
-      <!-- 标题 -->
-      <h1 class="flex items-center w-full tracking-widest lg:text-3xl">
-        关于光点计划
-        <span class="inline-block w-24 h-0.5 mx-5 bg-black"></span> 方 向 介 绍
-      </h1>
-      <!-- 返回按钮 -->
-      <div
-        class="flex items-center gap-2 px-2 transition-all group shrink-0 text-zinc-400 lg:hover:-translate-y-1 lg:hover:scale-105"
-      >
-        <svg
-          class="icon fill-zinc-400 group-hover:fill-sky-400"
-          aria-hidden="true"
-        >
-          <use xlink:href="#icon-home"></use>
-        </svg>
-        <button class="group-hover:text-sky-400">返回首页</button>
-      </div>
-    </div>
+    <HeadTopNav>方 向 介 绍</HeadTopNav>
     <!-- 导航栏 -->
     <div class="fixed w-32 text-sm lg:left-10 text-slate-400">
       <p class="py-2 border-b text-sky-400 border-sky-400">目录</p>
@@ -31,17 +11,16 @@
           :key="index"
           class="overflow-hidden leading-loose hover:text-sky-400 whitespace-nowrap text-ellipsis"
         >
-          <a :href="`#project${index}`">{{ item }}</a>
+          <a :href="`#${item}`">{{ item }}</a>
         </li>
       </ul>
     </div>
-
     <!-- 方向介绍正文 -->
     <div
-      class="w-4/5 p-4 mt-8 transition-all bg-white hover:scale-[1.02] lg:ml-48 myShadow"
+      class="w-4/5 p-4 mt-8 transition-all bg-white hover:scale-[1.02] lg:ml-52 myShadow"
       v-for="(item, index) in projectInfo"
       :key="index"
-      :id="`project${index}`"
+      :id="`${item.name}`"
     >
       <!-- 标题 -->
       <h1 class="text-2xl font-bold tracking-wider text-sky-400">
@@ -52,11 +31,11 @@
         <!-- 头衔与联系方式 -->
         <div class="flex justify-between">
           <p class="font-bold tracking-widest">{{ item?.leader?.name }}</p>
-          <p class="flex gap-4">
-            <span class="px-2 bg-teal-200 rounded-full">{{
+          <p class="flex items-center gap-4">
+            <span class="px-2 text-sm bg-teal-200 rounded-full">{{
               item?.leader?.title
             }}</span>
-            <span class="px-2 bg-pink-200 rounded-full">{{
+            <span class="px-2 text-sm bg-pink-200 rounded-full">{{
               item?.leader?.grade
             }}</span>
           </p>
@@ -99,13 +78,21 @@
             v-for="(projectItem, index) in item?.projectIntroduction?.paragraph"
             :key="index"
           >
-            <p v-if="index % 2 === 0">
+            <!-- 小标题 -->
+            <p v-if="projectItem.title">
               <span class="inline-block w-2 h-2 bg-teal-400 rounded-lg"></span
-              >&nbsp; {{ projectItem }}
+              >&nbsp; {{ projectItem.title }}
             </p>
-            <p v-else class="my-1 whitespace-pre-wrap indent-4 text-slate-500">
-              {{ projectItem }}
-            </p>
+            <!-- 内容 -->
+            <div v-if="projectItem.content.length">
+              <p
+                v-for="(content, index) in projectItem.content"
+                :key="index"
+                class="my-1 whitespace-pre-wrap indent-4 text-slate-500"
+              >
+                {{ content }}
+              </p>
+            </div>
           </li>
         </ul>
         <p class="my-1 indent-4" v-if="item?.projectIntroduction?.overview">
@@ -117,10 +104,32 @@
         <p><span class="font-bold">🔔 注意事项: </span>{{ item?.notice }}</p>
       </div>
     </div>
+    <!-- Q & A -->
+    <div
+      class="w-4/5 p-4 mt-8 transition-all bg-white hover:scale-[1.02] lg:ml-52 myShadow"
+      id="Q&A"
+    >
+      <h1 class="text-2xl font-bold tracking-wider text-sky-400">🎯 Q & A</h1>
+      <div class="p-2 mt-2 border-t">
+        <div v-for="(item, index) in FAQ" :key="index" class="pb-2">
+          <p class="font-bold">
+            <span class="pr-3 font-normal text-sky-400">{{
+              `${index + 1}.`
+            }}</span
+            >{{ item?.question }}
+          </p>
+          <p class="indent-4">{{ item?.answer }}</p>
+        </div>
+      </div>
+    </div>
+    <CopyRights></CopyRights>
   </div>
 </template>
 
 <script setup>
+import HeadTopNav from "@/components/HeadTopNav.vue";
+import CopyRights from "@/components/CopyRights.vue";
+
 const projectName = [
   "分布式系统与云计算",
   "编译原理",
@@ -151,12 +160,24 @@ const projectInfo = [
       name: "基于选主机制的master高可用mapreduce并行计算框架",
       foreword: "不要觉得这个项目很难，我们拆开来看",
       paragraph: [
-        "什么是并行计算",
-        "简单来说，就是把一个完整的计算任务交给多个计算机完成，以此提高效率。一种简单的实现就是把大任务拆成多个小任务，分派给不同计算机执行，再合并执行结果。",
-        "首先我们要实现一个mapreduce并行计算框架",
-        "mapreduce是什么嘞，简单来说它是一个谷歌提出来的一种实现分布式计算的方法，它实现起来相对简单，通过master和worker的形式组织集群成员，把任务拆分分派，最后收集合并。我们会学习google当年的论文，尝试实现能让计算并行化的简单框架。",
-        "其次呢我们要实现一个选主机制，来完成master的高可用",
-        "什么意思呢，我们实现的master只有一只，而worker有一堆，论文中也讲了worker挂了会怎么处理。那么如果master挂了呢？我们将再使用分布式系统的一些常用方法，比如选主机制，来完成master的高可用。",
+        {
+          title: "什么是并行计算",
+          content: [
+            "简单来说，就是把一个完整的计算任务交给多个计算机完成，以此提高效率。一种简单的实现就是把大任务拆成多个小任务，分派给不同计算机执行，再合并执行结果。",
+          ],
+        },
+        {
+          title: "首先我们要实现一个mapreduce并行计算框架",
+          content: [
+            "mapreduce是什么嘞，简单来说它是一个谷歌提出来的一种实现分布式计算的方法，它实现起来相对简单，通过master和worker的形式组织集群成员，把任务拆分分派，最后收集合并。我们会学习google当年的论文，尝试实现能让计算并行化的简单框架。",
+          ],
+        },
+        {
+          title: "其次呢我们要实现一个选主机制，来完成master的高可用",
+          content: [
+            "什么意思呢，我们实现的master只有一只，而worker有一堆，论文中也讲了worker挂了会怎么处理。那么如果master挂了呢？我们将再使用分布式系统的一些常用方法，比如选主机制，来完成master的高可用。",
+          ],
+        },
       ],
       overview:
         "总而言之，我们会通过实现项目目标和论文学习的方式，逐步去尝试mapreduce之类一些简单的分布式系统的设计思想，实现一个简单能跑的小系统。总体而言不会很困难，但也需要一定时间的论文学习和调试过程。",
@@ -183,12 +204,28 @@ const projectInfo = [
       name: "自定义语言编译器设计",
       foreword: "",
       paragraph: [
-        "编译前端",
-        "1. 读懂文法，完成自定义语言的词法分析、语法分析。可选择flex+bison（C/C++）或者Antlr（Java）完成\n2. 设计你需要的简单的数据结构，比如符号表、基本块、函数等，推荐参照llvm的设计方式，当然也可以用简单的三代码设计。\n3. 遍历语法树，实现中间代码生成。",
-        "编译中端",
-        "1. SSA化（可选择完成）\n2. 中间代码优化（常量传播、公共子表达式、死代码删除、函数内联等等），优化是没有尽头的，欢迎同学们深入尝试。",
-        "编译后端",
-        "1. 将中间代码翻译成目标汇编代码，架构的话默认选择ARM架构，对RISCV很感兴趣的同学，也可使用RISCV架构。\n2. 寄存器分配（可选，如果时间紧迫但又想尝试，比起图着色，我们更推荐线性扫描分配）",
+        {
+          title: "编译前端",
+          content: [
+            "1. 读懂文法，完成自定义语言的词法分析、语法分析。可选择flex+bison（C/C++）或者Antlr（Java）完成",
+            "2. 设计你需要的简单的数据结构，比如符号表、基本块、函数等，推荐参照llvm的设计方式，当然也可以用简单的三代码设计。",
+            "3. 遍历语法树，实现中间代码生成。",
+          ],
+        },
+        {
+          title: "编译中端",
+          content: [
+            "1. SSA化（可选择完成）",
+            "2. 中间代码优化（常量传播、公共子表达式、死代码删除、函数内联等等），优化是没有尽头的，欢迎同学们深入尝试。",
+          ],
+        },
+        {
+          title: "编译后端",
+          content: [
+            "1. 将中间代码翻译成目标汇编代码，架构的话默认选择ARM架构，对RISCV很感兴趣的同学，也可使用RISCV架构。",
+            "2. 寄存器分配（可选，如果时间紧迫但又想尝试，比起图着色，我们更推荐线性扫描分配）",
+          ],
+        },
       ],
       overview: "",
     },
@@ -215,21 +252,47 @@ const projectInfo = [
       foreword:
         "根据ISA设计一个简易处理器，并自己编写汇编代码检测设计的处理器实现的正确性。",
       paragraph: [
-        "电路描述语言学习:可以选择verilog或者是chisel硬件描述语言。尝试学习使用硬件描述语言描述多路选择器以及加法器。",
-        "",
-        "仿真环境的使用:学习使用verilator，用仿真环境运行上面两个设计，通过观察波形检查实现的正确性。",
-        "",
-        "ISA以及微架构学习:学习了解将要设计的ISA特性，以及单周期处理器的架构。",
-        "",
-        "设计处理器:通过硬件描述语言设计该处理器。",
-        "",
-        "仿真验证:自己编写测试用汇编代码，用仿真器进行仿真，检测处理器实现的正确性。",
-        "",
+        {
+          title:
+            "电路描述语言学习:可以选择verilog或者是chisel硬件描述语言。尝试学习使用硬件描述语言描述多路选择器以及加法器。",
+          content: [],
+        },
+        {
+          title:
+            "仿真环境的使用:学习使用verilator，用仿真环境运行上面两个设计，通过观察波形检查实现的正确性。",
+          content: [],
+        },
+        {
+          title:
+            "ISA以及微架构学习:学习了解将要设计的ISA特性，以及单周期处理器的架构。",
+          content: [],
+        },
+        {
+          title: "设计处理器:通过硬件描述语言设计该处理器。",
+          content: [],
+        },
+        {
+          title:
+            "仿真验证:自己编写测试用汇编代码，用仿真器进行仿真，检测处理器实现的正确性。",
+          content: [],
+        },
       ],
       overview: "",
     },
     notice:
       "作为一个项目来说，需要培养自主学习能力。对软件工程的学生来说，数字电路相关知识了解很少。虽然但是，网上的学习资源很多，可以查找学习，同时该项目涉及的深度不深，只要花时间就可以学会。仿真环境是该项目唯一涉及软件的部分，需要大家对C++有一定的了解，会一些基础的C++编程。当然你也可以用C，但是需要正确处理verilator的C++部分。",
+  },
+];
+const FAQ = [
+  {
+    question: "项目大概需要多久来完成？",
+    answer:
+      "使用课余时间完成，大约一个月的时间，尽力完成即可，毕竟优化是没有尽头的。",
+  },
+  {
+    question: "项目如何提交？",
+    answer:
+      "和光点计划要求一致，实现的代码通过 PR 的形式上传到 github 或国产开源平台，同时在学习过程中记录下自己的心得体会、实现思路和测试截图等，以项目笔记的方式上传。",
   },
 ];
 </script>
