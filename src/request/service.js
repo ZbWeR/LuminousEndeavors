4ae9586d550ae5f3c76fc58e3f32702f47f3b6fd
ZbWeR <1641064392@qq.com>
2023-08-13 20:16:49 +0800

    ✨ feat: axios拦截失败请求

--- src/request/service.js
+++ src/request/service.js
@@ -1,32 +1,33 @@
 import axios from "axios";
-import store from "@/store";
 
 function createService(){
     const service = axios.create({
         baseURL: 'http://1.15.179.24:9520',
         timeout: 5000,
         headers: {
+            "Accept":"*/*",
             "Content-Type": "application/json",
         },
     });
     // 请求拦截
     service.interceptors.request.use(
         // 获取 token
-        (config) => {
-            // const token = store.state.token;
-            // token && (config.headers.Authorization = token);
-            return config;
-        },
+        (config) => config,
         (error) => Promise.reject(error)
     )
     // 响应拦截
     service.interceptors.response.use(
-        (response) => {
+        (response) => { 
             const apiData = response.data;
-            console.log(apiData);
+            const {code,message} = apiData;
+            if(code !== 200){
+                console.log(message);
+                throw new Error(message);
+            }
+            return response;
         },
         (error) => {
-            // console.log(error);
+            console.log(error);
             Promise.reject(error);
         }
     )
