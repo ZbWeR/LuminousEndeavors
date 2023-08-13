020041cbd364d7e045518cf57bc56bddad712b2a
ZbWeR <1641064392@qq.com>
2023-08-13 20:17:25 +0800

    ✨ feat: 封装注册相关接口

--- src/request/api/auth.js
+++ src/request/api/auth.js
@@ -1,15 +1,50 @@
 import service from "../service";
 
-export function SendVerifyCode(data) {
+/**
+ * 发送短信验证码,无需token
+ * @param {Number}  phoneNumber - 电话号码
+ * @returns 包含响应数据的Promise对象
+ */
+export function SendVerifyCode(phoneNumber) {
     return service({
         method: 'POST',
-        url: `/user/sendCode`,
+        url: `/user/sendCodeToPhone`,
         data:{
-            "phoneNumber":15359586391
+            phoneNumber
+        }
+    })
+}
+/**
+ * 验证短信验证码
+ * @param {Number} code - 用户输入的验证码
+ * @param {String} tempKey - 短信验证临时标志
+ * @returns 包含响应数据的Promise对象
+ */
+export function verifyPhoneNumber(code,tempKey){
+    return service({
+        method:'POST',
+        url:'/user/verifyCode',
+        data:{
+            code,tempKey
         }
     })
 }
 
+/**
+ * 获取图形验证码
+ * @returns 包含响应数据的Promise对象
+ */
+export function getCaptchaImage(){
+    return service({
+        method:'GET',
+        url:'/captchaImage'
+    })
+}
+
+/**
+ * 验证图形验证码
+ * @returns 
+ */
 export function verifyCaptchaImage(){
     return service({
         method: 'POST',
@@ -18,5 +53,18 @@ export function verifyCaptchaImage(){
             "uuid":"299d134dbabd4e099d88b10bc09334ab",
             "codeOfUser":"32"
         }
+    })
+}
+
+/**
+ * 提交注册信息
+ * @param {Object} data - 包含注册信息的对象 
+ * @returns 包含响应数据的Promise对象
+ */
+export function register(data){
+    return service({
+        method:'POST',
+        url:'/user/register',
+        data:data
     })
 }
