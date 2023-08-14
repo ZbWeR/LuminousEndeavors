214be254b5b26871207ef8c04b694cbab42656cb
ZbWeR <1641064392@qq.com>
2023-08-14 21:27:00 +0800

    ✨ feat: 添加组件过渡动画

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        opacityHidden:{
+          '0':{opacity:1},
+          '100%':{
+            transform:'translateY(-100px) translateX(-50%)',
+            opacity:0
+          }
+        }
+      },
+      animation:{
+        messageAnimation: 'opacityHidden 1s'
+      }
+    },
   },
   plugins: [],
   darkMode: 'class',
+
 }
