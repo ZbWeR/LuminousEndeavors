c8ecbad899859876be13d328055de036eed62d88
ZbWeR <1641064392@qq.com>
2023-08-14 21:26:39 +0800

    🧪 test: 测试 message 组件

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/ProjectIntroduction.vue')
   },
   {
+    path: '/test',
+    name: 'test',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
     path: '/more',
     name: 'more',
     // route level code-splitting
