6a45b0e3aa1790c3b971ff115d5faa80f69fef8f
ZbWeR <1641064392@qq.com>
2023-08-24 17:04:28 +0800

    ✨ feat: 绑定手机

--- src/components/ResetPassword.vue
+++ src/components/ResetPassword.vue
@@ -4,9 +4,11 @@
     class="absolute z-40 flex items-center justify-center w-screen h-screen overflow-hidden bg-black/50"
   >
     <div class="px-5 py-4 bg-white rounded-md lg:w-1/4">
-      <h1 class="text-xl font-bold tracking-widest text-sky-400">重置密码</h1>
+      <h1 class="text-xl font-bold tracking-widest text-sky-400">
+        {{ showPhone ? "绑定手机" : "重置密码" }}
+      </h1>
       <!-- 密码 -->
-      <div class="w-full mt-2">
+      <div v-if="!showPhone" class="w-full mt-2">
         <input
           class="transition-all duration-300 w-full px-2 py-1 outline-none focus:shadow-[0_0_0_1px_#409eff] rounded shadow-[0_0_0_1px_#dcdfe6]"
           type="password"
@@ -15,7 +17,7 @@
         />
       </div>
       <!-- 重复密码 -->
-      <div class="w-full mt-2">
+      <div v-if="!showPhone" class="w-full mt-2">
         <input
           class="transition-all duration-300 w-full px-2 py-1 outline-none focus:shadow-[0_0_0_1px_#409eff] rounded shadow-[0_0_0_1px_#dcdfe6]"
           type="password"
@@ -51,7 +53,7 @@
         </button>
       </div>
       <button
-        @click="handleReset"
+        @click="handleSet"
         :disabled="!verifyInfo.code"
         class="block w-1/4 px-2 py-2 mx-auto mt-3 text-sm text-white transition-all duration-300 bg-teal-400 rounded-full disabled:cursor-not-allowed disabled:bg-red-200 hover:bg-teal-500"
       >
@@ -62,10 +64,18 @@
 </template>
 
 <script setup>
-// TODO:根据传入props的值确认类型
+// TODO:根据传入props的值确认类型,不传入props但是解析了props会发生什么？
 import { reactive, ref } from "vue";
 import { SendVerifyCode, verifyPhoneNumber } from "@/request/api/auth";
 import { resetUserInfo } from "@/request/api/info";
+
+const { funType, hasPhone, token } = defineProps([
+  "funType",
+  "hasPhone",
+  "token",
+]);
+const showPhone = funType === "phone" && !hasPhone; // 绑定手机功能
+console.log(funType, hasPhone, showPhone);
 
 // 输入信息
 const inputData = reactive({
@@ -80,7 +90,7 @@ const verifyInfo = reactive({
   hasSent: false,
 });
 
-const emit = defineEmits(["alert", "toggleShow"]);
+const emit = defineEmits(["alert", "toggleShow", "updateToken"]);
 const sendCodeBtn = ref("60s 后重试");
 // 发送验证码
 async function sendCode() {
@@ -105,34 +115,54 @@ async function sendCode() {
     return;
   }
 }
-// 提交重置信息
-async function handleReset() {
+
+async function handleSet() {
   try {
-    // 检验非空
-    let allValuesNotEmpty = Object.values(inputData).every(
-      (item) => item !== ""
-    );
-    if (!allValuesNotEmpty) {
-      emit("alert", "请填写完毕所有内容", "error");
-      return;
+    // 重置信息校验
+    if (!showPhone) {
+      // 检验非空
+      let allValuesNotEmpty = Object.values(inputData).every(
+        (item) => item !== ""
+      );
+      if (!allValuesNotEmpty) {
+        emit("alert", "请填写完毕所有内容", "error");
+        return;
+      }
+      // 检验两次密码是否一致
+      if (inputData.password !== inputData.passwordAgain) {
+        emit("alert", "两次密码不一致", "error");
+        return;
+      }
     }
-    // 检验两次密码是否一致
-    if (inputData.password !== inputData.passwordAgain) {
-      emit("alert", "两次密码不一致", "error");
-      return;
+    // 绑定手机校验
+    else {
+      if (inputData.phoneNumber === "") {
+        emit("alert", "请填写完毕所有内容", "error");
+        return;
+      }
     }
+
     // 校验验证码
     let { data } = await verifyPhoneNumber(
       verifyInfo.code,
       verifyInfo.tempKey,
       inputData.phoneNumber
     );
-    let token = data?.data;
+    let codeToken = data?.data;
+    // 绑定手机号
+    if (showPhone) {
+      await resetUserInfo(token, inputData.phoneNumber);
+      emit("alert", "绑定成功！", "success");
+      emit("toggleShow", inputData.phoneNumber);
+    }
     // 重置用户密码
-    await resetUserInfo(token, inputData.phoneNumber, inputData.password);
-    emit("alert", "重置成功！", "success");
-    emit("toggleShow");
+    else {
+      await resetUserInfo(codeToken, inputData.phoneNumber, inputData.password);
+      emit("alert", "重置成功！", "success");
+      emit("toggleShow");
+    }
   } catch {
+    emit("toggleShow");
     return;
   }
 }
