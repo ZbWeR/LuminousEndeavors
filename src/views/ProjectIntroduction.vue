041527da8081f7ddcef93969180780ede8e8e2b1
ZbWeR <1641064392@qq.com>
2023-08-25 19:38:07 +0800

    ✨ feat: 调整样式 & 更新FAQ

--- src/views/ProjectIntroduction.vue
+++ src/views/ProjectIntroduction.vue
@@ -17,7 +17,7 @@
     </div>
     <!-- 方向介绍正文 -->
     <div
-      class="w-4/5 p-4 mt-8 transition-all duration-300 bg-white hover:scale-[0.98] lg:ml-52 myShadow"
+      class="leading-relaxed w-4/5 p-4 mt-8 transition-all duration-300 bg-white hover:scale-[0.98] lg:ml-52 myShadow ml-36"
       v-for="(item, index) in projectInfo"
       :key="index"
       :id="`${item.name}`"
@@ -47,13 +47,13 @@
           </p>
         </div>
         <!-- 个人经历 -->
-        <p class="text-slate-500">
+        <p class="mt-1 text-slate-500">
           {{ item?.leader?.experience }}
         </p>
       </div>
       <!-- 方向简介 -->
-      <div class="mt-2 border-b">
-        <h1 class="text-lg text-sky-400">🎨 方向简介</h1>
+      <div class="pb-1 mt-2 border-b">
+        <h1 class="pb-1 text-xl font-bold text-sky-400">🎨 方向简介</h1>
         <p
           class="my-1 indent-4"
           v-for="(directionItem, index) in item.directionIntroduction"
@@ -63,9 +63,9 @@
         </p>
       </div>
       <!-- 项目简介 -->
-      <div class="mt-2">
-        <div class="flex items-center justify-between p-2">
-          <h1 class="text-lg text-sky-400">📝 项目简介</h1>
+      <div class="pb-1 mt-2 border-b">
+        <div class="flex items-center justify-between">
+          <h1 class="pb-1 text-xl font-bold text-sky-400">📝 项目简介</h1>
           <p class="font-bold">
             {{ item?.projectIntroduction?.name }}
           </p>
@@ -77,6 +77,7 @@
           <li
             v-for="(projectItem, index) in item?.projectIntroduction?.paragraph"
             :key="index"
+            class="pb-1"
           >
             <!-- 小标题 -->
             <p v-if="projectItem.title">
@@ -99,27 +100,22 @@
           {{ item?.projectIntroduction?.overview }}
         </p>
       </div>
-      <!-- 注意事项 -->
-      <div class="p-2 mt-2 bg-teal-100 border-2 border-teal-500 rounded">
-        <p><span class="font-bold">🔔 注意事项: </span>{{ item?.notice }}</p>
-      </div>
-    </div>
-    <!-- Q & A -->
-    <div
-      class="w-4/5 p-4 mt-8 transition-all bg-white hover:scale-[0.98] duration-300 lg:ml-52 myShadow"
-      id="Q&A"
-    >
-      <h1 class="text-2xl font-bold tracking-wider text-sky-400">🎯 Q & A</h1>
-      <div class="p-2 mt-2 border-t">
-        <div v-for="(item, index) in FAQ" :key="index" class="pb-2">
-          <p class="font-bold">
-            <span class="pr-3 font-normal text-sky-400">{{
-              `${index + 1}.`
-            }}</span
-            >{{ item?.question }}
+      <!-- FAQ -->
+      <div class="mt-2">
+        <h1 class="pb-1 text-xl font-bold text-sky-400">🎯 常见问题</h1>
+        <div v-for="(FAQItem, index) in item.FAQ" :key="index">
+          <h2 class="pt-1 pb-1 font-bold">Q: {{ FAQItem.question }}</h2>
+          <p class="text-slate-600">
+            <span class="font-bold">A:</span> {{ FAQItem.answer }}
           </p>
-          <p class="indent-4">{{ item?.answer }}</p>
         </div>
+      </div>
+      <!-- 注意事项 -->
+      <div
+        v-if="item?.notice"
+        class="p-2 mt-4 bg-teal-100 border-2 border-teal-500 rounded"
+      >
+        <p><span class="font-bold">🔔 注意事项: </span>{{ item?.notice }}</p>
       </div>
     </div>
     <CopyRights></CopyRights>
@@ -136,7 +132,6 @@ const projectName = [
   "处理器设计",
   "计算机视觉与机器学习",
   "操作系统",
-  "Q&A",
 ];
 
 const projectInfo = [
@@ -147,7 +142,7 @@ const projectInfo = [
       title: "微光工作室副主席",
       grade: "2021级嵌入式方向",
       experience:
-        "目前在字节跳动视频架构部门实习，主要方向为分布式系统、云计算与运维开发。项目成果有CubeUniverse云原生海量数据存储、MiniDocker、GlimmerMeeting等。曾获第十三届中国大学生服务外包创新创业大赛全国二等奖，在创新创业类、算法类、多媒体类等竞赛中拥有获奖经历。",
+        "目前在字节跳动视频架构部门实习，主要方向为分布式系统、云计算与运维开发。项目成果有CubeUniverse云原生存储平台、MiniDocker、GlimmerMeeting等。曾获第十三届、第十四届中国大学生服务外包创新创业大赛全国二等奖，在创新创业类、算法类、多媒体类等竞赛中拥有获奖经历。",
       contact: "2071594767",
     },
     directionIntroduction: [
@@ -184,6 +179,22 @@ const projectInfo = [
     },
     notice:
       "本项目基于go语言实现，考虑到同学可能没有接触过go语言，本方向的前期工作将围绕语言基础做一些准备，因此不需要担心因为没学过go而影响项目进度。同时go语言本身足够简单，且它的应用也很广泛，所以也不用怕学不懂或是学了没用。总之，大一0基础的小伙伴尝试也是没问题的。",
+    FAQ: [
+      {
+        question: "学习本方向项目能让我获得什么？",
+        answer:
+          "除了光点计划给予的免试加入工作室和其他奖励，可以用实践的方式收获一个分布式方向的小项目，为未来申请各种东西打基础，且能提高go语言的代码水平，并对本方向涉及的内容作一个初探。而且我觉得本方向实践起来非常有意思，做出一个能实际利用的东西也非常有成就感。",
+      },
+      {
+        question: "项目大概需要多久来完成？",
+        answer: "使用课余时间完成，三个星期到一个月内应该可以搞定",
+      },
+      {
+        question: "项目如何提交？",
+        answer:
+          "和光点计划要求一致，实现的代码通过PR的形式上传到github或国产开源平台，同时在学习过程中记录下自己的心得体会、实现思路和测试截图等，以项目笔记的方式上传。",
+      },
+    ],
   },
   {
     name: "编译原理",
@@ -230,7 +241,24 @@ const projectInfo = [
       overview: "",
     },
     notice:
-      "本项目不要求大家完全杀穿，可选择的完成语言有C、C++、Java。在完成编译前端生成中间代码之后，可选择中间代码优化、后端代码翻译两个方向中的一个继续完成即可，不要求全部做完，尽力就好。希望同学们通过该项目对编译有一个整体上的认识，当然，期待同学们的大展身手。",
+      "本项目不要求大家完全杀穿，可选择的完成语言有C、C++、Java。在完成编译前端生成中间代码之后，可选择编译中端、编译后端两个方向中的一个继续完成即可（不了解ARM或RISCV架构的同学推荐完成部分中端代码优化，也可以选择只完成编译后端的寄存器分配），不要求全部做完，尽力就好。希望同学们通过该项目对编译有一个整体上的认识，当然，期待同学们的大展身手。",
+    FAQ: [
+      {
+        question: "学习本方向项目能让我获得什么？",
+        answer:
+          "除了光点计划给予的免试加入工作室和其他奖励，可以深入编译世界的奥秘，大二上恰好有编译原理的课程，能完成这个项目的同学相信可以薄纱学校的课程了。此外，这个从0开始完成的项目能锻炼你项目构建的能力、提高你的数据结构设计能力，且能提高所选语言的代码水平。当然，最希望你能从这个项目中收获成就感与快乐。",
+      },
+      {
+        question: "项目大概需要多久来完成？",
+        answer:
+          "使用课余时间完成，大约一个月的时间，尽力完成即可，毕竟优化是没有尽头的。",
+      },
+      {
+        question: "项目如何提交？",
+        answer:
+          "和光点计划要求一致，实现的代码通过PR的形式上传到github或国产开源平台，同时在学习过程中记录下自己的心得体会、实现思路和测试截图等，以项目笔记的方式上传。",
+      },
+    ],
   },
   {
     name: "处理器设计",
@@ -281,6 +309,24 @@ const projectInfo = [
     },
     notice:
       "作为一个项目来说，需要培养自主学习能力。对软件工程的学生来说，数字电路相关知识了解很少。虽然但是，网上的学习资源很多，可以查找学习，同时该项目涉及的深度不深，只要花时间就可以学会。仿真环境是该项目唯一涉及软件的部分，需要大家对C++有一定的了解，会一些基础的C++编程。当然你也可以用C，但是需要正确处理verilator的C++部分。",
+    FAQ: [
+      {
+        question: "学习本方向项目能让我获得什么？",
+        answer:
+          "除了光点计划给予的免试加入工作室和其他奖励，可以用实践的方式收获一个 处理器方向的小项目，为未来申请各种东西打基础。其次，让你从一个全新视角观察一个计算机是如何工作，对软件的执行有着更加深刻的认识。或许，对以后如果想学习嵌入式会有所帮助。",
+      },
+
+      {
+        question: "项目大概需要多久来完成？",
+        answer:
+          "视相关基础而定，基础很好甚至可以一天完成；对0基础来说，一个月内也是可以完成的。",
+      },
+      {
+        question: "如何炫耀我强大的模型？",
+        answer:
+          "和光点计划要求一致，实现的代码通过PR的形式上传到github或国产开源平台，同时在学习过程中记录下自己的心得体会、实现思路和测试截图等，以项目笔记的方式上传。",
+      },
+    ],
   },
   {
     name: "计算机视觉与机器学习",
@@ -289,7 +335,7 @@ const projectInfo = [
       title: "微光工作室副主席",
       grade: "2021级系统与技术方向",
       experience:
-        "于2023年完成景点推荐平台的设计与实现，于2022年服务创新创业大赛中协助完成垃圾智能分类识别的项目。除此之外，在众多比赛中独立完成了 WEB 前端+ WEB 后端的开发任务，并在2022年中国高校计算机大赛-网络技术挑战赛中荣获国家级一等奖。",
+        "目前主要的研究方向是NLP、推荐系统。于2023年完成景点推荐平台的设计与实现，于2022年服务创新创业大赛中协助完成垃圾智能分类识别的项目。同时在众多比赛中独立完成了全栈的开发任务。",
       contact: "1370172202",
     },
     directionIntroduction: [
@@ -327,18 +373,83 @@ const projectInfo = [
       overview: "",
     },
     notice: "测评平台 URL （尚未开放）",
+    FAQ: [
+      {
+        question: "学习本方向项目能让我获得什么？",
+        answer:
+          "本题类似于不太正规的打榜任务，完成本题可以初步体会如何进行指标类的评测，感受到刺激的活动氛围和排名上升的喜悦感，为后续进行正规、大型的比赛做铺垫。在技术上，能够在不断优化过程中了解更多的图像分类模型，并逐渐熟练掌握模型的训练流程和技巧，是一次难得的代码训练机会。",
+      },
+      {
+        question: "完成本题与免试进入工作室有什么关联？",
+        answer:
+          "实现本题可以不用完成常规 ML 招新题来获得面试机会，且我们参考纳入面试名单的因素有很多，不限制于排名。如果你成功进入面谈环节，请熟悉自己使用的模型以应对面试可能进行的相关提问。最后会综合考量比拼和面谈的表现来选择获得免试进入工作室资格的人选。",
+      },
+      {
+        question: "项目大概需要多久来完成？",
+        answer: "在光点计划开展的时间段内尽力完成即可，毕竟优化是没有尽头的。",
+      },
+      {
+        question: "如何炫耀我强大的模型？",
+        answer:
+          "本着光点计划是一个开源项目计划，且本题为招新趣味题，测评者可以将自己的使用的模型、调整的参数、 md 介绍文件上传至自己的 GitHub 仓库，然后在测评平台的备注中写入自己的仓库链接供大家参考",
+      },
+    ],
   },
-];
-const FAQ = [
   {
-    question: "项目大概需要多久来完成？",
-    answer:
-      "使用课余时间完成，大约一个月的时间，尽力完成即可，毕竟优化是没有尽头的。",
-  },
-  {
-    question: "项目如何提交？",
-    answer:
-      "和光点计划要求一致，实现的代码通过 PR 的形式上传到 github 或国产开源平台，同时在学习过程中记录下自己的心得体会、实现思路和测试截图等，以项目笔记的方式上传。",
+    name: "操作系统",
+    leader: {
+      name: "李佳轩",
+      title: "微光工作室成员",
+      grade: "2021级系统与技术方向",
+      experience:
+        "主要研究方向是计算机体系结构，操作系统。代表项目有GlimmerOS操作系统。曾获2022C4网络挑战赛全国一等奖。",
+      contact: "1206370306",
+    },
+    directionIntroduction: [
+      "说到操作系统，不知道大家对挂在嘴边的windows是否有所了解，它就是我们最广为熟知操作系统。操作系统是计算机系统中至关重要的组成部分，负责管理计算机的各种资源，以及协调不同程序之间的执行，就连你目前查看我们官网的硬件屏幕也离不开操作系统的管理。通过学习操作系统设计，你将了解到你的计算机是如何工作的：任务调度，硬件的抽象……，同时可以给你对于计算机的学习带来一个崭新的思考角度。",
+    ],
+    projectIntroduction: {
+      name: "操作系统简易功能设计",
+      foreword:
+        "根据xv6的简易操作系统，增加一些格外系统功能，并自己编译检验功能实现的正确性.",
+      paragraph: [
+        {
+          title: "下载一个自己的linux虚拟机（装主机上也可以）",
+          content: [],
+        },
+        {
+          title: "下载一个自己的IDE并配置",
+          content: [],
+        },
+        {
+          title: "下载qemu并且下载xv6源码",
+          content: [],
+        },
+        {
+          title: "根据题目实现目标功能",
+          content: [],
+        },
+      ],
+      overview: "",
+    },
+    notice: "",
+    FAQ: [
+      {
+        question: "学习本方向项目能让我获得什么？",
+        answer:
+          "除了光点计划给予的免试加入工作室和其他奖励，可以让你对操作系统有一个入门级的认识，并且可以当作一个小项目，为未来申请各种东西打基础。其次，让你从一个全新视角观察一个计算机是如何工作，对软件的执行有着更加深刻的认识。",
+      },
+      {
+        question: "项目难嘛，我应该如何去学习它？",
+        answer:
+          "项目本身是很有难度的，但是如果能实现它，会给你带来很多收获。并且对自身的设计能力也是有巨大的提升。为了学习它，需要你自己寻找相关书籍和课程进行学习，同时，我也会给一些相关架构的资料。",
+      },
+      {
+        question: "项目如何提交？",
+        answer:
+          "和光点计划要求一致，实现的代码通过PR的形式上传到github或国产开源平台，同时在学习过程中记录下自己的心得体会、实现思路和测试截图等，以项目笔记的方式上传",
+      },
+    ],
   },
 ];
 </script>
