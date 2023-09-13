db1ca4612bb99e0147b3fb7327d920f2c2ba41f1
ZbWeR <1641064392@qq.com>
2023-09-13 23:05:30 +0800

    ✨ feat: 尝试利用 cookie 进行跨站通信

--- src/store/token.js
+++ src/store/token.js
@@ -1,14 +1,18 @@
-const tokenName = 'token';
+const tokenName = "token";
 
-// 设置token
-export function setToken(newToken){
-    localStorage.setItem(tokenName,newToken);
+// 设置token,保存到本地与cookie
+export function setToken(newToken) {
+  localStorage.setItem(tokenName, newToken);
+  document.cookie = `token=${newToken}; domain=glimmer.org.cn; path=/; Secure`;
+  if (document.cookie) console.log("-- set success --");
 }
 // 获取token
-export function getToken(){
-    return localStorage.getItem(tokenName);
+export function getToken() {
+  return localStorage.getItem(tokenName);
 }
 // 删除token
-export function removeToken(){
-    localStorage.removeItem(tokenName);
+export function removeToken() {
+  localStorage.removeItem(tokenName);
+  document.cookie =
+    "token=; domain=glimmer.org.cn; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; Secure";
 }
